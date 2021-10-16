bfffeeec3c64d122cdedefccd621ef55182f364a
Mike Starbuck <miglas9@gmail.com>
2021-10-16 16:31:39 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
-function App() {
+
+const colors = {
+  0: "white",
+  1: "red",
+  2: "green",
+  3: "blue",
+  4: "yellow"
+  // 5: "orange"
+}
+
+const App = (props) => {
+
+  const [color, setColor] = React.useState();
+  const synthRef = React.useRef(window.speechSynthesis);
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    finalTranscript,
+    browserSuppoertsRecognition
+  } = useSpeechRecognition();
+  React.useEffect(() => {
+    setColor(0);
+  }, [])
+
+  React.useEffect(() => {
+    SpeechRecognition.stopListening();
+    setTimeout(() => {
+      if (finalTranscript == "") {
+        return;
+      }
+      if (finalTranscript === colors[color]) {
+        correct();
+      }
+      else {
+        incorrect();
+      }
+    }, 200);
+    resetTranscript();
+  }, [finalTranscript])
+
+  function handleClick() {
+    let number = Math.floor((Math.random() * 4)) + 1
+    setColor(number);
+    SpeechRecognition.startListening({ continuous: true });
+    // if (finalTranscript != "") {
+    //   if (finalTranscript === colors[number])
+    //     alert("You did it")
+    // }
+  }
+  const correct = () => {
+    const word = "correct";
+    const utterThis = new SpeechSynthesisUtterance(word);
+    utterThis.rate = 1.0;
+    setTimeout(() => {
+      synthRef.current.speak(utterThis);
+    }, 500);
+  }
+
+  const incorrect = () => {
+    const word = `Incorrect.  The color is ${colors[color]}.`;
+    const utterThis = new SpeechSynthesisUtterance(word);
+    utterThis.rate = 1.0;
+    setTimeout(() => {
+      synthRef.current.speak(utterThis);
+    }, 500);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"
+      style={
+        {
+          backgroundColor: colors[color],
+          height: 100 + '%'
+        }}
+      onClick={handleClick}
+    >
+      Click the screen to generate a random color!!
+      <p>{finalTranscript}</p>
     </div>
   );
 }
